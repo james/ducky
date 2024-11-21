68aed0c586e0e708a924b3c60972fd9d530fecb2
James Darling <james@abscond.org>
2024-11-21 18:43:20 +0000

    A single, ERB'd prompt
    I ran the generated prompts though... Claude AI. After a bit of chat, we decided that a single user prompt was better than multiple system and user prompts. Claude rewrote it like this. I haven't had a chance to test it out yet as I finished getting it working in an internetless cabin, but I think now's a good time to do a git commit, and then tweak it in future commits.
    This also has the advantage of being easier to see the whole thing, and then use ERB instead of string interpolation.

--- journal.rb
+++ journal.rb
@@ -2,6 +2,7 @@ require 'json'
 require 'date'
 require 'io/console'
 require 'ruby/openai'
+require 'erb'
 
 data_folder = File.join(__dir__, 'data')
 
@@ -10,33 +11,6 @@ data_json_path = File.join(data_folder, 'data.json')
 data_json = File.exist?(data_json_path) ? JSON.parse(File.read(data_json_path)) : {}
 
 client = OpenAI::Client.new(access_token: data_json['openai']['apiKey'])
-
-# Build messages array for GPT
-messages = []
-
-# Add meta prompt
-messages << {
-  role: 'system',
-  content: "You are a chatbot named DUCKY. Your job is to help James Darling do daily journaling. " \
-    "You're going to be keeping track of the questions asked and the answers given. " \
-    "You are generally helpful and friendly."
-}
-
-messages << {
-  role: 'system',
-  content: "VERY IMPORTANT: Today is #{Time.now.strftime('%A')} and the date is #{Time.now.strftime('%d/%m/%Y')}, " \
-  "the time is #{Time.now.strftime('%H:%M:%S')}. Important: Saturday and Sunday are the weekend for " \
-  "relaxing not working, if the day is Saturday or Sunday make your questions less work related and more " \
-  "time-off chilling related. #{data_json['name']} counts the start of the week and work week as Monday, " \
-  "anything 'new week' related starts on Monday. The end of 'work week' related ends on Friday. " \
-  "IMPORTANT, please don't be asking about the weekend on any other day than Monday or Friday, please pay " \
-  "attention to the current day of the week."
-}
-
-messages << {
-  role: 'system',
-  content: "Here is a list of default questions to get you started:\n#{data_json['morningQuestions'].join("\n")}"
-}
 
 # Load previous questions and answers
 qa_file = File.join(data_folder, 'previousQuestionsAndAnswers.json')
@@ -58,13 +32,15 @@ end
 
 # Format previous Q&As
 today = Date.today.iso8601
+qa_text = ""
+
 last_five_dates.each do |date|
   next unless questions_and_answers['answers'] && questions_and_answers['answers'][date]
 
   days_ago = (Date.parse(today) - Date.parse(date)).to_i
   day_of_week = Date.parse(date).strftime('%A')
 
-  qa_text = case days_ago
+  qa_text << case days_ago
     when 0
       "Today, #{day_of_week} (#{date}), #{data_json['name']}'s Q&As were:\n"
     when 1
@@ -80,35 +56,13 @@ last_five_dates.each do |date|
     qa_text += "A#{i + 1}: #{qa['answer']}\n"
   end
   qa_text += "\n"
-
-  messages << {
-    role: 'system',
-    content: qa_text
-  }
 end
 
 todolist = `reminders show "To do"`
-  messages << {
-    role: 'system',
-    content: "James has a todo list, and on his todo list currently is the following:\n" + todolist
-  }
 
-# Request new questions from GPT
-messages << {
-  role: 'user',
-  content: "Please create a set of three questions to ask in the MORNING by combining relevant questions " \
-  "from the initial list and formulating new ones. When appropriate, incorporate overarching themes from " \
-  "previous responses into the questions, but, very important, don\'t be overly direct with follow-up " \
-  "inquiries and don\'t always reference them, you can go back to the original list of questions for guidance, " \
-  "please mix it up, one question should always be new and unreliated to previous answers. " \
-  "Remember the information about weekends! Put more weight on recent questions and answers rather than " \
-  "older ones, questions and answers from a few days ago are less relevant than the past couple of days " \
-  "(unless the last couple of days we the weekend). Put more focus on the answers given than the questions " \
-  "you previously asked (they are just there for context). " \
-  "Remember you are asking these at the start of the day. There must be three questions, no more or less. " \
-  "Please return those three questions in .txt format with one question per line. " \
-  "Important: there must be NO numbers at the start of each question! Only return the questions no other text."
-}
+template = File.read('prompt.erb')
+prompt = ERB.new(template).result_with_hash(questions_and_answers: qa_text, todolist: todolist)
+messages = [{ role: 'user', content: prompt }]
 
 # Print messages
 puts messages.map { |m| m[:content] }.join("\n\n")
